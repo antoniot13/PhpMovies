3c2f300b71f9d50d65412b9d6bca315df3e8b218
Antonio Todorov <antonio.todorov13@gmail.com>
2017-02-11 16:34:30 +0100

    SingleMovie view
    Trying to commit

---
+++ resources/views/singlemovie.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+
+                    <img src="http://placehold.it/300x350" alt="">
+                </div>
+                <div class="col-md-7 col-md-offset-1">
+                    <p>Focus is a 2015 American romantic crime comedy-drama film
+                        written and directed by Glenn Ficarra and John Requa, starring Will Smith, Margot Robbie, and Rodrigo Santoro.
+                        The film was released on February 27, 2015 and received mixed reviews
+                        from critics but was a success at the box office, grossing a total of
+                        $159 million off its $50 million dollar budget</p>
+                    <hr>
+                    
+
+                </div>
+
+
+
+
+            </div>
+    </div>
+@endsection
