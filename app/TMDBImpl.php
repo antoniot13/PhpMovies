419ada15afd27bdd81eeab83fe9e2283993c5b26
Mirche Toshevski <mirche.toshevski@gmail.com>
2017-02-13 21:50:31 +0100

    ratings fix 2

--- app/TMDBImpl.php
+++ app/TMDBImpl.php
@@ -18,8 +18,12 @@ class TMDBImpl {
 
         foreach($list["results"] as $temp) {
             $ourRating = DBImpl::getRatingsForMovie($temp["id"]);
+            $rating = $temp["vote_average"];
+            if ($ourRating != 0) {
+                $rating = ($ourRating + $rating) / 2;
+            }
             $m = new Movie($temp["title"], $temp["id"], $temp["poster_path"], $temp["overview"], $temp["popularity"],
-                $temp["vote_count"], ($temp["vote_average"] + $ourRating) / 2, $temp["release_date"], $temp["genre_ids"],
+                $temp["vote_count"], $rating, $temp["release_date"], $temp["genre_ids"],
                 'http://image.tmdb.org/t/p/w500');
             array_push($movies_array, $m);
         }
